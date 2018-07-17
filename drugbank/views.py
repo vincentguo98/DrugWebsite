aafb4c72ce148b5f4ccee6f5f0bab7b8a4e36f0e
JerryWXJ <39091294+JerryWXJ@users.noreply.github.com>
2018-07-17 21:22:51 +0800

    V

--- drugbank/views.py
+++ drugbank/views.py
@@ -9,42 +9,64 @@ from django.shortcuts import render
 from drugbank import models
 from search_class import *
 from drugbank.models import *
-from django.http import JsonResponse,HttpResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
-option = Option(projection_=["primaryDrugbankId", "name"],order_=["name","primaryDrugbankId"])
-
 
 
 @csrf_exempt
 def ProjectionResult(request):
-	
-	lis = request.POST.get("drug")
-	druglist = eval(lis)
-	print(lis)
-	option = Option(projection_ = eval(lis))
-	querydrug = QueryDrug(option)
-	querydrug.setQueryModel(Drug)
-	drug = querydrug.parse()
-	print(drug)
-	return JsonResponse(drug)
+    lis = request.POST.get("drug")
+    druglist = eval(lis)
+    print(lis)
+    option = Option(projection_=eval(lis))
+    # querydrug = QueryDrug(option)
+    # querydrug.setQueryModel(Drug)
+    # drug = querydrug.parse()
+    # print(drug)
+    # return JsonResponse(drug)
+    return HttpResponse("1")
+
 
 @csrf_exempt
 def index(request):
-	return render(request,'drugbank/index.html',locals())
+    return render(request, 'drugbank/index.html', locals())
 
 
 @csrf_exempt
 def search(request):
-	field_name = []
-	for field in Drug._meta.fields:
-		print(field.verbose_name)
-		field_name.append({"field_item_name":field.verbose_name.replace(' ','_')})
-	field_dict = {"name":field_name}
-	return render(request,'drugbank/search.html',context=field_dict)
-
-
+    druglist = [Drug, Pathway, Category, Carrier, Dosage, SnpAdverseDrugReaction, Synonym, SnpEffect, Salt, PathwayDrug,
+                Property, Mixture, Reaction]
+    drugbank_dict = {
+        "Drug": Drug,
+        "Pathway": Pathway,
+        "Category": Category,
+        "Carrier": Carrier,
+        "Dosage": Dosage,
+        "SnpAdverseDrugReaction": SnpAdverseDrugReaction,
+        "Synonym": Synonym,
+        "SnpEffect": SnpEffect,
+        "Salt": Salt,
+        "PathwayDrug": PathwayDrug,
+        "Property": Property,
+        "Mixture": Mixture,
+        "Reaction": Reaction
+    }
+    field_name = []
+    context = []
+    for key, value in drugbank_dict.items():
+        for field in value._meta.fields:
+            field_name.append(field.verbose_name.replace(' ', '_'))
+        filed_dict = {key: field_name}
+        field_name = []
+        context.append(filed_dict)
+    dict_example = {"context": context}
+    for table in dict_example["context"]:
+        for key, value in table.items():
+            print(key)
+            for i in value:
+                print(key, '--', i)
+    return render(request, 'drugbank/search.html', context={"context": context})
 
 #
 # if __name__ == '__main__':
@@ -61,5 +83,4 @@ def search(request):
 # 	print("-"*20)
 # 	for field in Drug._meta.fields:
 # 		print(field.verbose_name)
-	
-	
+
