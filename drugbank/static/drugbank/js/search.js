6fd64831f3fa00353b2d02e65234dbd645838c86
rpSebastian <cxxuhang@126.com>
2018-08-14 11:46:45 +0800

    save

--- drugbank/static/drugbank/js/search.js
+++ drugbank/static/drugbank/js/search.js
@@ -1,6 +1,8 @@
 var index = new Array();
 var datax;
 $(document).ready(function () {
+    $("#drugbank-drug-type-label").html("drug type");
+    $("#drugbank-drug-group-label").html("drug group");
     $(".minop").click(function () {
         if (this.value == "0") {
             index.push(this.id);
@@ -38,12 +40,60 @@ $(document).ready(function () {
     $("#drugbank-drug-div").on("change", function () {
         $("#drugbank-drug-div option").each(function () {
             val = $(this).html();
+            if (val == "drug-drug interaction")
+                val = "drug-drug-interaction";
             if (val != "smile" && val != "inchi") {
                 if ($(this).get(0).selected) {
                     $("#drugbank-" + val + "-div").show();
                 } else {
                     $("#drugbank-" + val + "-div").hide();
                 }
+            }
+        });
+    });
+    $(".download").on('click', function(){
+        cond = {};
+        cond.drug = []
+        cond.drug_group = []
+        $("#drugbank-drug-div option").each(function () {
+            val = $(this).html();
+            if ($(this).get(0).selected){
+                cond.drug.push(val);
+            }
+        });
+        $("#drugbank-drug-type-div option").each(function () {
+            val = $(this).html();
+            if ($(this).get(0).selected){
+                cond.drug_type = val;
+            }
+        });
+        $("#drugbank-drug-group-div option").each(function () {
+            val = $(this).html();
+            if ($(this).get(0).selected){
+                cond.drug_group.push(val);
+            }
+        });
+        $("#drugbank-fingerprint-div option").each(function () {
+            val = $(this).html();
+            if ($(this).get(0).selected){
+                if (val == "yes")
+                    cond.fingerprint = 1 ;
+                else
+                    cond.fingerprint = 0;
+            }
+        });
+        $("#drugbank-download-div option").each(function () {
+            val = $(this).html();
+            if ($(this).get(0).selected){
+                cond.download = val;
+            }
+        });
+        $.ajax({
+            type: "post",
+            url: "/test",
+            data: cond,
+            dataType: "json",
+            success: function (response) {
             }
         });
     });
